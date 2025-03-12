a750e5cb7e78e46f49065906714784011cc7bdef
Luiz Kim <luiz.kim@controleonline.com>
2025-03-12 16:43:11 -0300

    Update

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,47 +1,29 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import SignInPage from "@controleonline/ui-login/src/react/pages/sign-in/index";
-import HomePage from "@controleonline/ui-shop/src/react/pages/home/index";
-import OrdersPage from "@controleonline/ui-orders/src/react/pages/orders/sales/index";
-import OrderDetails from "@controleonline/ui-orders/src/react/pages/orders/sales/orderDetails";
-import AddProductToOrder from "@controleonline/ui-products/src/react/components/products/addProductToOrder";
-import Checkout from "@controleonline/ui-orders/src/react/components/checkout/index";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import loginRoutes from '@controleonline/ui-login/src/react/router/routes';
+import shopRoutes from '@controleonline/ui-shop/src/react/router/routes';
+import ordersRoutes from '@controleonline/ui-orders/src/react/router/routes';
+import productsRoutes from '@controleonline/ui-products/src/react/router/routes';
 
 const Stack = createNativeStackNavigator();
 
-export default function Routes() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name="SignInPage"
-                component={SignInPage}
-                options={{ headerShown: false }}
-            />
-            <Stack.Screen
-                name="HomePage"
-                component={HomePage}
-                options={{ headerShown: false, title: 'Menu' }}
-            />
-            <Stack.Screen
-                name="SalesOrdersPage"
-                component={OrdersPage}
-                options={{ headerShown: true, title: 'Pedidos de Venda', headerBackButtonMenuEnabled: false }}
-            />
-            <Stack.Screen
-                name="OrderDetails"
-                component={OrderDetails}
-                options={{ headerShown: true, title: 'Detalhes do pedido' }}
-            />
-            <Stack.Screen
-                name="AddProductToOrder"
-                component={AddProductToOrder}
-                options={{ headerShown: true, title: 'Adicionar Produto ao Pedido' }}
-            />
-            <Stack.Screen
-                name="Checkout"
-                component={Checkout}
-                options={{ headerShown: true, title: 'Forma de Pagamento' }}
-            />
+const allRoutes = [
+  ...loginRoutes,
+  ...shopRoutes,
+  ...ordersRoutes,
+  ...productsRoutes,
+];
 
-        </Stack.Navigator>
-    );
+export default function Routes() {
+  return (
+    <Stack.Navigator>
+      {allRoutes.map((route, index) => (
+        <Stack.Screen
+          key={index}
+          name={route.name}
+          component={route.component}
+          options={route.options}
+        />
+      ))}
+    </Stack.Navigator>
+  );
 }
