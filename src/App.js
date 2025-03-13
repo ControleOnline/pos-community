73a9a25975eca3681470b6656f7bc1687dc56b1e
Luiz Kim <luiz.kim@controleonline.com>
2025-03-13 17:05:27 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import {NavigationContainer} from '@react-navigation/native';
 // import { Provider } from 'react-redux';
 import Routes from './routers';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-window.LocalStorage = AsyncStorage;
 import {api} from '@controleonline/ui-common/src/api';
+window.localStorage = AsyncStorage;
 window.api = api;
 
 import {ThemeProvider} from '@controleonline/ui-layout/src/react/components/ThemeProvider';
