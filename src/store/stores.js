640139fda07259b46cd15e71aa40e85a2e96dc51
Luiz Kim <luiz.kim@controleonline.com>
2025-04-24 16:00:47 -0300

    Update

--- src/store/stores.js
+++ src/store/stores.js
@@ -7,6 +7,7 @@ import status from '@controleonline/ui-common/src/store/status';
 import device from '@controleonline/ui-common/src/store/device';
 import device_config from '@controleonline/ui-common/src/store/device_config';
 import print from '@controleonline/ui-common/src/store/print';
+import printer from '@controleonline/ui-common/src/store/printer';
 import translate from '@controleonline/ui-translate/src/store/translate';
 import auth from '@controleonline/ui-login/src/store/modules/auth';
 import cart from '@controleonline/ui-orders/src/store/cart';
@@ -53,6 +54,7 @@ export default {
   translate,
   theme,
   print,
+  printer,
   address,
   documents,
   phones,
