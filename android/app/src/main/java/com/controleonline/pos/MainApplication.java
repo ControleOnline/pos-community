143e7e089f5fcab93a259fe4fc9d777baa65d3d3
Luiz Kim <luiz.kim@controleonline.com>
2026-01-09 11:40:06 -0300

    Changing path os lib

--- android/app/src/main/java/com/controleonline/pos/MainApplication.java
+++ android/app/src/main/java/com/controleonline/pos/MainApplication.java
@@ -1,6 +1,7 @@
 package com.controleonline.pos;
 
 import android.app.Application;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
@@ -8,14 +9,14 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
+
 import java.util.List;
-import com.controleonline.pos.cielo.payment.CieloPackage;
-import com.controleonline.pos.infinitepay.payment.InfinitePayPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
       new DefaultReactNativeHost(this) {
+
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
@@ -23,10 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          packages.add(new CieloPackage());
-          packages.add(new InfinitePayPackage());
           return packages;
         }
 
@@ -54,10 +52,15 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+    SoLoader.init(this, false);
+
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       DefaultNewArchitectureEntryPoint.load();
     }
-    ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+
+    ReactNativeFlipper.initializeFlipper(
+      this,
+      getReactNativeHost().getReactInstanceManager()
+    );
   }
 }
