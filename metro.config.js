3e92ac35ad88afb949d6e3cccc56956db1bd13ff
Luiz Kim <luiz.kim@controleonline.com>
2026-02-01 00:38:46 -0300

    Expo

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,25 @@
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
-const path = require("path");
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {
+const defaultConfig = getDefaultConfig(__dirname);
+
+module.exports = {
+  ...defaultConfig,
   resolver: {
+    ...defaultConfig.resolver,
     extraNodeModules: {
+      ...defaultConfig.resolver.extraNodeModules,
       "@store": path.resolve(__dirname, "src/store"),
     },
+    sourceExts: [...defaultConfig.resolver.sourceExts, 'cjs'],
+  },
+  transformer: {
+    ...defaultConfig.transformer,
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
   },
 };
-
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
